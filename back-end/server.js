02a1ae05d06987b6040c87c159534302e0754e79
s-a-malik <shreshth_malik@yahoo.co.uk>
2022-01-20 00:08:39 +0000

    task object schema

--- back-end/server.js
+++ back-end/server.js
@@ -17,16 +17,37 @@ app.use(cors());
 app.use(express.json());
 
 
-// create a database to store each task
-const tasks = {};
-
+/* create an object to store each task
+each task is a json object with the following fields:
+- taskId: unique id for the task (same as index of the array)
+- numLabelsRequired: number of labels required *per example* to complete the task
+- contractAddress: address of the smart contract associated with the task
+- funds: funds available to be spent on the task (dynamically updated on contract event)
+- examples: an array of images to be displayed to the user (TODO URLs?)
+- labelOptions: dict of names of possible labels (keys) and their values (unique id)
+- labels: an array of dicts(address:label) of the submitted labels
+- numLabellers: number of labellers who have submitted labels for this task
+- consensus: the consensus label for each image
+- payout: payout dict for each account (created when consensus is reached)
+- status: the status of the task (open, expired, or completed)
+- created: the time the task was created
+- expiry: the time the task expires
+- setter: the public address that created the task
+*/
+const tasks = [];
 
 // show the tasks available to the front end
-app.get('/balance/:address', (req, res) => {
+app.get('/tasks', (req, res) => {
   const {address} = req.params;
   const balance = balances[address] || 0;
   res.send({ balance });
 });
+
+
+// create a new task
+app.post('/tasks/create-task', (req, res) => {
+    // get address confirmed from metamask
+    // const {funds, examples, labelOptions, expiry} = req.body;
 
 
 // serves images in a random order to the front end inside a task
@@ -35,33 +56,36 @@ app.get('/balance/:address', (req, res) => {
 // TODO need to make sure that the images are not repeated to the same user
 
 
+// submit labels to server after labelling in front end
+// // TODO need to encrypt this to send across internet?
+// app.post('tasks/:task/submit-labels', (req, res) => {
+//   console.log('Received a batch of labels...');
+//   // TODO need user's address to be verified - sign with metamask
 
 
-// TODO need to encrypt this to send across internet?
-app.post('/send', (req, res) => {
-  console.log('Received a batch of labels...');
-  // TODO need user's address to be verified - sign with metamask
-  const {sender, recipient, amount, signature} = req.body;
-  // verify request using sender public key and signature
-  bodyToVerify = JSON.stringify({
-    sender, amount, recipient
-  });
-  const key = ec.keyFromPublic(sender, 'hex');
-  const bodyHash = SHA256(bodyToVerify).toString();
-  const valid = key.verify(bodyHash, signature);
-  console.log(`Signature is ${valid ? 'valid' : 'invalid'}`);
-  if (!valid) {
-    console.log('Transaction failed!');
-    res.status(400).send('Invalid signature');
-  }
-  else {
-    // update sender balance
-    balances[sender] -= amount;
-    balances[recipient] = (balances[recipient] || 0) + +amount;
-    console.log(`Updated balances`);
-    res.send({ balance: balances[sender] });
-  }
-});
+// app.post('/send', (req, res) => {
+//   const {sender, recipient, amount, signature} = req.body;
+//   // verify request using sender public key and signature
+//   bodyToVerify = JSON.stringify({
+//     sender, amount, recipient
+//   });
+//   const key = ec.keyFromPublic(sender, 'hex');
+//   const bodyHash = SHA256(bodyToVerify).toString();
+//   const valid = key.verify(bodyHash, signature);
+//   console.log(`Signature is ${valid ? 'valid' : 'invalid'}`);
+//   if (!valid) {
+//     console.log('Transaction failed!');
+//     res.status(400).send('Invalid signature');
+//   }
+//   else {
+//     // update sender balance
+//     balances[sender] -= amount;
+//     balances[recipient] = (balances[recipient] || 0) + +amount;
+//     console.log(`Updated balances`);
+//     res.send({ balance: balances[sender] });
+//   }
+// });
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
