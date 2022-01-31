0516bea027e6bd34c7d97680f97134a526867359
Ollie Turnbull <oturnbull1@gmail.com>
2022-01-31 21:27:20 +0000

    Added contracts artefacts

--- front-end/src/TaskCreation.js
+++ front-end/src/TaskCreation.js
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form'
 import ConnectWalletButton from './Components/ConnectWalletButton'
-
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import "./TaskCreation.css";
+import Settlement from './contracts/Settlement.json'
+import Task from './contracts/Task.json'
 // import contract from './contracts/Creater.json';
 import { ethers } from 'ethers';
 import {
@@ -25,12 +29,20 @@ const bytecode = '';
 const backendURL = '';
 
 
+const schema = yup.object({
+  taskName: yup.string().required(),
+  taskDescription : yup.string().required(),
+  expiryDate : yup.date().required()
+}).required();
+
 function TaskCreation() {
   // rinkeby test network
   const provider = new ethers.providers.Web3Provider(window.ethereum, "rinkeby");
 
   // form
-  const {handleSubmit, register, formState: {errors, isSubmitting},} = useForm();
+  const {handleSubmit, register, formState: {errors, isSubmitting},} = useForm({
+    resolver: yupResolver(schema)
+  });
 
   async function postToBackend(values) {
     
@@ -39,16 +51,16 @@ function TaskCreation() {
       contractAddress : values.deployedAddress,
       setter : provider.getSigner().getAddress(),
       created : Date.now(),
-      expiry : values['expiry-date'],
-      funds : values['funds-assigned']
+      expiry : values['expiryDate'],
+      funds : values['funds']
     };
 
     const taskInfo = {
-      taskName : values['task-name'],
-      taskDesription : values['task-description'],
+      taskName : values['taskName'],
+      taskDesription : values['taskDescription'],
       example : values['example'],
-      numLabelsRequired : values['num-labels'],
-      labelOptions : values['ptins'], 
+      numLabelsRequired : values['numLabels'],
+      labelOptions : values['options'], 
       status : "active"
     }
 
@@ -68,24 +80,31 @@ function TaskCreation() {
         body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
 
-    const content = await rawResponse.json();
-    return content;
+    //const content = await rawResponse.json();
+    //return content;
   }
 
-
-
+  
+  // Deploys contracts using user values in form. Calls postToBackend.
+  // TODO #9 handle if user runs out of funds after deploying contracts, before funding
+  // TODO Handle user rejecting transaction
   async function deployContract(values)
   { 
-    // deploy a new Task contract
-    const Task = await new ethers.ContractFactory(abi, bytecode, provider.getSigner());
-    const task = await Task.deploy()
+    // first deploy settlement
+    alert(values.funds);
+    const SettlementFactory = new ethers.ContractFactory(Settlement.abi, Settlement.bytecode, provider.getSigner());
+    const settlement = await SettlementFactory.deploy();
+    await settlement.deployed();
+    // deploy task contract
+    const TaskFactory =  new ethers.ContractFactory(Task.abi, Task.bytecode, provider.getSigner());
+    const task = await TaskFactory.deploy(settlement.address);
     await task.deployed();
-    const deployedAddress = task.address;
-    values.deployedAddress = deployedAddress
-
-    // TODO send funds to contract
-
+    // send funds to task
+    task.deposit({value: ethers.utils.parseEther((values.funds).toString())});
+    values.settlementAddress = settlement.address;
+    values.taskAddress = task.address;
     postToBackend(values);
+    console.log(values);
 
   }
 
@@ -100,15 +119,15 @@ function TaskCreation() {
 // TODO form validation with Yup
   return (
     <div className='task-creation'>
-      <ConnectWalletButton/>
+      <ConnectWalletButton className="connect-wallet"/>
       <h1>Task Creation</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl isRequired>
-        <FormLabel htmlFor='task-name'>Task Name</FormLabel>
-        <Input id='task-name' placeholder='Task Name' {...register('task-name', {
+        <FormLabel htmlFor='taskName'>Task Name</FormLabel>
+        <Input id='taskName' placeholder='Task Name' {...register('taskName', {
             required: true })} />
-        <FormLabel htmlFor='task-description'>Task Description</FormLabel>
-        <Input id='task-name' placeholder='Task Name' {...register('task-description', {
+        <FormLabel htmlFor='taskDescription'>Task Description</FormLabel>
+        <Input id='taskName' placeholder='Task Name' {...register('taskDescription', {
             required: true,
             })}/>
         <FormLabel htmlFor='images'>Images</FormLabel>
@@ -118,29 +137,33 @@ function TaskCreation() {
             required: true,
             })} />
         </InputGroup>
-        <FormLabel htmlFor='num-labels'>Number of labels per task</FormLabel>
+        <FormLabel htmlFor='numLabels'>Number of labels per task</FormLabel>
         <NumberInput min={1}>
-          <NumberInputField id='num-lables' {...register('num-labels', {
+          <NumberInputField id='num-lables' {...register('numLabels', {
             required: true,
             })} />
         </NumberInput>
-        <FormLabel htmlFor='funds-assigned'>Funds assigned for payout (Gwei)</FormLabel>
+        <FormLabel htmlFor='funds'>Funds assigned for payout (Eth)</FormLabel>
         <InputGroup>
-          <NumberInput min={1}>
-            <NumberInputField id='funds-assigned' {...register('funds-assigned', {
+          <NumberInput>
+            <NumberInputField id='funds' {...register('funds', {
             required: true,
             })}/>
           </NumberInput>
-          <InputRightAddon children='Gwei' />
+          <InputRightAddon children='Eth' />
         </InputGroup>
         <FormLabel htmlFor='example'>Example Labelling</FormLabel>
-        <Input id='example' placeholder='Task Name' {...register('eample', {
+        <InputGroup size='sm'>
+          <InputLeftAddon children='https://' />
+          <Input placeholder='example.com' {...register('example', {
+            required: true,
+            })} />
+        </InputGroup>
+        <FormLabel htmlFor='expiryDate'>Expiry Date</FormLabel>
+        <Input id='expiryDate' placeholder='12/05/22' {...register('expiryDate', {
             required: true,
             })}/>
-        <FormLabel htmlFor='expiry-date'>Expiry Date</FormLabel>
-        <Input id='expiry-date' placeholder='12/05/22' {...register('expiry-date', {
-            required: true,
-            })}/>
+          <p>{errors.expiryDate?.message}</p>
         <FormLabel htmlFor='options'>Labelling Options</FormLabel>
         <Input id='options' placeholder='labelling options' {...register('options', {
             required: true,
